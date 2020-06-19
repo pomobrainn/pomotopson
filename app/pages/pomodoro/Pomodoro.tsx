0fbb20073061da120d90eb677499796ff3b1be54
João Antonio Maruti Milagres <joaoantoniomaruti@gmail.com>
2020-06-19 17:39:10 -0300

    create actions buttons

--- app/pages/pomodoro/Pomodoro.tsx
+++ app/pages/pomodoro/Pomodoro.tsx
@@ -4,11 +4,34 @@ import Timer from 'react-compound-timer';
 
 const Pomodoro = () => {
   const initialTime = 25 * 60 * 1000;
+
   return (
     <>
-      <Timer initialTime={initialTime} direction="backward">
-        <Timer.Minutes /> minutes
-        <Timer.Seconds /> seconds
+      <Timer
+        initialTime={initialTime}
+        direction="backward"
+        checkpoints={[
+          {
+            time: 0,
+            callback: () => console.log('Checkpoint A')
+          }
+        ]}
+      >
+        {({ start, resume, pause, stop }: any) => (
+          <React.Fragment>
+            <div>
+              <Timer.Minutes /> minutes
+              <Timer.Seconds /> seconds
+            </div>
+            <br />
+            <div>
+              <button onClick={start}>Start</button>
+              <button onClick={pause}>Pause</button>
+              <button onClick={resume}>Resume</button>
+              <button onClick={stop}>Stop</button>
+            </div>
+          </React.Fragment>
+        )}
       </Timer>
     </>
   );
